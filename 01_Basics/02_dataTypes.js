3a0d41d8e3ba9c2e87729cfa96a04c6c5505e6a3
Host-Vedas <147248574+Host-Vedas@users.noreply.github.com>
2024-02-18 10:16:06 +0000

    data types done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict "; // Treat all JS code as a newer version
+
+// alert(3+3) it gives error in nodeJs engine but work finely on browser
+
+
+let name = "jayant"
+let age = 18
+let isLoggedIn = false
+
+/* */
